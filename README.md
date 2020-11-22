a1d5b9fe6f6a83b5aa9e7b5aa3de8ba33f1d1af6
Gowrish15 <gowrishvicky@gmail.com>
2020-11-22 18:15:43 +0530

    new

---
+++ README.md
@@ -0,0 +1,4 @@
+# SyncEquip
+
+An all new mobile application to manage your assests in the most effective way ever
+
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:app_usage/app_usage.dart';
+
+void main()
+{
+
+}
