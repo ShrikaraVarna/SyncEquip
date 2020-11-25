4ef3a1689b797c7d563a86f36490619672d82962
Gowrish15 <gowrishvicky@gmail.com>
2020-11-25 16:13:41 +0530

    d

--- lib/addNewpage.dart
+++ lib/addNewpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/demo.dart';
 
 // Demonstrates how to use autofill hints. The full list of hints is here:
 // https://github.com/flutter/engine/blob/master/lib/web_ui/lib/src/engine/text_editing/autofill_hint.dart
@@ -9,6 +10,79 @@ class addNewpage extends StatefulWidget {
 
 class _addNewpageState extends State<addNewpage> {
   final _formKey = GlobalKey<FormState>();
+
+  Widget _scanButton() {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Demo()),
+        );
+      },
+
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(vertical: 10),
+        margin: EdgeInsets.symmetric(horizontal: 60),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Colors.grey.shade200,
+                  offset: Offset(2, 4),
+                  blurRadius: 5,
+                  spreadRadius: 2)
+            ],
+            gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Colors.brown[300], Colors.brown[400]])),
+
+        child: Text(
+          'Scan QR Code',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+
+      ),
+    );
+  }
+
+  Widget _submitButton() {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Demo()),
+        );
+      },
+
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(vertical: 15),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Colors.grey.shade200,
+                  offset: Offset(2, 4),
+                  blurRadius: 5,
+                  spreadRadius: 2)
+            ],
+            gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Colors.lightBlue[300], Colors.lightBlue[400]])),
+
+        child: Text(
+          'Add Device',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,7 +142,10 @@ class _addNewpageState extends State<addNewpage> {
                         labelText: 'Location- Room No.',
                       ),
                     ),
-
+                    SizedBox(height: 20),
+                    _scanButton(),
+                    SizedBox(height: 20),
+                    _submitButton(),
                   ].expand(
                         (widget) => [
                       widget,
