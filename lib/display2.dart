0c52a0efa9a5cf4b76ddfc586295837d73c2c82d
Gowrish15 <gowrishvicky@gmail.com>
2020-11-26 16:44:36 +0530

    dis

--- lib/display2.dart
+++ lib/display2.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:SyncEquip/auth.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'crud.dart';
+import 'dart:ui' as ui;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:path/path.dart';
@@ -23,16 +24,39 @@ class _display2State extends State<display2> {
     return new Scaffold(
       appBar: AppBar(
         title: Text('Device Info'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              dialogTrigger(context);
-            },
-          )
-        ],
       ),
-      body: devListDisplay(),
+      body: Center(
+        child:Column(
+          children: <Widget>[
+            Text(
+                'View Devices',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                  foreground: Paint()
+                    ..shader = ui.Gradient.linear(
+                      const Offset(0, 75),
+                      const Offset(150, 75),
+                      <Color>[
+                        Colors.lightBlueAccent,
+                        Colors.deepPurple,
+                      ],
+                    ),
+                fontFamily: 'Schyler',
+                fontFamilyFallback: <String>[
+                  'Noto Sans CJK SC',
+                  'Noto Color Emoji',
+                ],
+              ),
+            ),
+            Expanded(
+                child: Container(
+                    child: devListDisplay()
+                ),
+            ),
+          ],
+        ),
+      ),
     );
     throw UnimplementedError();
   }
@@ -71,12 +95,54 @@ class _display2State extends State<display2> {
     if(devlist!=null)
       {
         return ListView.builder(
-          itemCount: devlist.documents.length,
+          itemCount: devlist.docs.length,
           padding: EdgeInsets.all(10.0),
           itemBuilder: (context,i){
-            return new ListTile(
-              title: Text(devlist.docs[i]['device_name']),
-              subtitle: Text(devlist.docs[i]['device_dept']),
+            return Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Card(
+                //clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                elevation: 5,
+                child: Column(
+                  children: <Widget>[ new ListTile(
+                title: Text( devlist.docs[i]['device_name']),
+                subtitle: Text(devlist.docs[i]['device_dept']),
+              ),
+
+                FlatButton(
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(15.0),
+                  ),
+                  child: Text('Location'),
+                  onPressed:() {
+                    showDialog(context: context,
+                        barrierDismissible: false,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text(
+                                "Location Details\nBuilding: "+devlist.docs[i]['dbuilding']+"\nFloor-"+devlist.docs[i]['dfloor']+
+                                    "\nRoom- "+devlist.docs[i].id,
+                                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+
+                            actions: <Widget>[
+                              FlatButton(child: Text('OK'),
+                                textColor: Colors.black
+                                , onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  color: Colors.greenAccent,
+                ),
+           ],
+                ),
+              ),
             );
           },
         );
