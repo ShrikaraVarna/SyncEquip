d89b1fd76b0526e09eebe85b91fa14548810784a
ShrikaraVarna <bshrikara.is18@bmsce.ac.in>
2020-11-29 01:33:26 +0530

    Modified Navigation

--- lib/mainpageAdmin.dart
+++ lib/mainpageAdmin.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:SyncEquip/addNewpage.dart';
 import 'auth.dart';
 import 'display2.dart';
-import 'package:SyncEquip/qrscan.dart';
+import 'package:SyncEquip/qrscanA.dart';
 
 import 'package:provider/provider.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -12,109 +12,125 @@ import 'package:firebase_database/firebase_database.dart';
 class MainPageAdmin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        title: Text(
-          "SyncEquip",
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.amberAccent
+    return new WillPopScope(
+      onWillPop: () async=> false ,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          centerTitle: false,
+          title: Text(
+            "SyncEquip",
+            style: TextStyle(
+              fontFamily:'Schyler',
+              fontSize: 40,
+              color: Colors.white,
+            ),
           ),
+          backgroundColor: Colors.cyan[400],
         ),
-        backgroundColor: Colors.blue,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget> [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.white, Colors.cyan[400]])),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget> [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)
+                    ),
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => addNewpage()));
+                    },
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+                      child: Text(
+                        "ADD NEW",
+                        style: TextStyle(
+                            color: Colors.cyan[500],
+                            fontSize: 36
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
               RaisedButton(
+
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50.0)
+                    borderRadius: BorderRadius.circular(10.0)
                 ),
                 onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => addNewpage()));
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => display2() ));
                 },
-                color: Colors.amber[600],
+
+                color: Colors.white,
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 33,horizontal: 33),
+                  padding: const EdgeInsets.symmetric(vertical: 20,horizontal: 20),
                   child: Text(
-                    "ADD NEW",
+
+                    "CHECK STATUS",
                     style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 36
+                        color: Colors.cyan[500],
+
+                        //fontWeight: FontWeight.bold,
+                        fontSize: 36
+                    ),
+                  ),
+                ),
+              ),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)
+                ),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) =>  QRScan()));
+                },
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+                  child: Text(
+                    "SCAN QR",
+                    style: TextStyle(
+                        color: Colors.cyan[500],
+                        //fontWeight: FontWeight.bold,
+                        fontSize: 36
+                    ),
+                  ),
+                ),
+              ),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)
+                ),
+                onPressed: (){
+                  context.read<Authenticate>().logout();
+                },
+                color: Colors.blue[600],
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+                  child: Text(
+                    "LOGOUT",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18
                     ),
                   ),
                 ),
               ),
             ],
           ),
-          RaisedButton(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50.0)
-            ),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => display2() ));
-            },
-            color: Colors.amber,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 33,horizontal: 33),
-              child: Text(
-                "CHECK STATUS",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 36
-                ),
-              ),
-            ),
-          ),
-          RaisedButton(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50.0)
-            ),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) =>  QRScan()));
-            },
-            color: Colors.amber[600],
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 33,horizontal: 33),
-              child: Text(
-                "SCAN QR",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 36
-                ),
-              ),
-            ),
-          ),
-          RaisedButton(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50.0)
-            ),
-            onPressed: (){
-              context.read<Authenticate>().logout();
-            },
-            color: Colors.indigo[600],
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 0,horizontal: 33),
-              child: Text(
-                "LOGOUT",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18
-                ),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
